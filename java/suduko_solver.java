5d19c49e19c673ba51e2d61bcbeae0bb53a73d57
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-16 16:18:51 +0530

    Implement Sudoku solver class

---
+++ java/suduko_solver.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, row, col, c)) {
+                            board[row][col] = c; // Place the digit
+                            
+                            if (solve(board)) return true; // Move to next empty cell
+                            
+                            board[row][col] = '.'; // Backtrack (reset)
+                        }
+                    }
+                    return false; // No digit 1-9 works here
+                }
+            }
+        }
+        return true; // All cells filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+            // Check column
+            if (board[i][col] == c) return false;
+            // Check 3x3 sub-box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
